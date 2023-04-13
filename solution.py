82d154766a582183f4f0043555868e87a4fab6d4
Jung1er <62351396+Jung1er@users.noreply.github.com>
2023-04-13 23:34:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,4 +6,4 @@ from hyppo.ksample import MMD
 chat_id = 392609262 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    return MMD(compute_kernel="rbf", gamma=1).test(x, y)[1] < 0.06
+    return MMD(compute_kernel="rbf", gamma=10).test(x, y)[1] < 0.06
