e84407b408dfdf3671713f9bc1cc1e049b7ca562
Jung1er <62351396+Jung1er@users.noreply.github.com>
2023-04-13 15:37:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,8 +6,4 @@ from scipy.stats import anderson_ksamp
 chat_id = 392609262 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    stat, cr_val, pval = anderson_ksamp([x, y])
-    if stat > cr_val[2]:
-        return True
-    else:
-        return False
+    return anderson_ksamp([x, y]).pvalue < 0.06
