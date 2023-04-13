5463b14bf053053757a584a5b90595f288c98cea
Jung1er <62351396+Jung1er@users.noreply.github.com>
2023-04-13 22:30:52 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import anderson_ksamp
+from hyppo.ksample import MMD
 
 
 chat_id = 392609262 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    return anderson_ksamp([x, y]).pvalue < 0.06
+    return MMD(compute_kernel="rbf", gamma=1).test(x, y)[1] < 0.06
