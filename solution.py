04b68894517dbdcead6112e09c932fc8d8a3222e
Jung1er <62351396+Jung1er@users.noreply.github.com>
2023-04-13 13:45:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 392609262 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    stat, cr_val, pval = anderson_ksamp([x, y])
+    if stat > cr_val[2]:
+        return True
+    else:
+        return False
